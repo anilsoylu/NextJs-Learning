07cb620a5ee5621919e0bdebcf91a30db6bdf132
Anıl Soylu <56033298+anilsoylu@users.noreply.github.com>
2022-11-29 07:49:18 +0300

    'use/nlayout/ncomponents'

---
+++ pages/product.js
@@ -0,0 +1,15 @@
+import Head from "next/head"
+
+function product() {
+  return (
+    <>
+      <Head>
+        <title>Product</title>
+        <meta name="description" content="bu sayfa ürün sayfasıdır." />
+      </Head>
+      <h1>product</h1>
+    </>
+  )
+}
+
+export default product
