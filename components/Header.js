a111ada772e4d356d78d5246de099eea72f340b1
Anıl Soylu <56033298+anilsoylu@users.noreply.github.com>
2022-11-29 10:07:09 +0300

    next link

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,52 @@
+import Link from "next/link"
+import { useRouter } from "next/router"
 import React from "react"
-import styles from "../styles/Header.module.css"
 
 function Header() {
+  const router = useRouter()
   return (
-    <>
-      <h1 className={styles.title}>Header</h1>
-      {/* <button className={styles.button}>Click</button> */}
-    </>
+    <div className="p-3 mb-3 border-bottom bg-dark">
+      <div className="container d-flex flex-wrap">
+        <ul className="nav me-auto">
+          <li>
+            <Link
+              href="/"
+              className={
+                router.pathname == "/"
+                  ? "nav-link px-2 link-secondary"
+                  : "nav-link px-2 text-white"
+              }
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/about"
+              className={
+                router.pathname == "/about"
+                  ? "nav-link px-2 link-secondary"
+                  : "nav-link px-2 text-white"
+              }
+            >
+              About
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/product"
+              className={
+                router.pathname == "/product"
+                  ? "nav-link px-2 link-secondary"
+                  : "nav-link px-2 text-white"
+              }
+            >
+              Product
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
   )
 }
 
