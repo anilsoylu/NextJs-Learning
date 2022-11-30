69656fe3da9ad4948ee30dd6062a723f9d1ebadf
Anıl Soylu <56033298+anilsoylu@users.noreply.github.com>
2022-11-30 19:11:19 +0300

    getServerSideProps

--- pages/user/[id].js
+++ pages/user/[id].js
@@ -2,48 +2,61 @@ import Head from "next/head"
 import { useRouter } from "next/router"
 import { useEffect, useState } from "react"
 
-function UserItem() {
-  const [user, setUser] = useState()
-  const router = useRouter()
+function UserItem({ userData }) {
+  //const [user, setUser] = useState()
+  // const router = useRouter()
 
-  const { id } = router.query
+  // const { id } = router.query
 
-  useEffect(() => {
-    const getData = async () => {
-      try {
-        const res = await fetch("https://jsonplaceholder.typicode.com/users")
-        const data = await res.json()
-        setUser(!!data && data.find((u) => u.id === Number(id)))
-      } catch (err) {
-        console.log(err)
-      }
-    }
-    getData()
-  }, [id])
+  // useEffect(() => {
+  //   const getData = async () => {
+  //     try {
+  //       const res = await fetch("https://jsonplaceholder.typicode.com/users")
+  //       const data = await res.json()
+  //       setUser(!!userData && userData.find((u) => u.id === Number(id)))
+  //     } catch (err) {
+  //       console.log(err)
+  //     }
+  //   }
+  //   getData()
+  // }, [id, userData])
 
   return (
     <div className="container">
       <Head>
-        <title>{user?.name}</title>
-        <meta name="description" content={user?.name} />
+        <title>{userData?.name}</title>
+        <meta name="description" content={userData?.name} />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <h1>User Item</h1>
 
       <p>
-        <b>Full Name : </b> {user?.name}
+        <b>Full Name : </b> {userData?.name}
       </p>
 
       <p>
-        <b>Email : </b> {user?.email}
+        <b>Email : </b> {userData?.email}
       </p>
 
       <p>
-        <b>Address : </b> {user?.address.street}
+        <b>Address : </b> {userData?.address.street}
       </p>
     </div>
   )
 }
 
 export default UserItem
+
+export const getServerSideProps = async (context) => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/users/${context.params.id}`
+  )
+  const userData = await res.json()
+
+  return {
+    props: {
+      userData,
+    },
+  }
+}
